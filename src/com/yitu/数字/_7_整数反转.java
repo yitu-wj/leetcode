11003784dd19009a2f294963938ed65d37bea6c7
yitu <2551434573@qq.com>
2021-11-04 22:03:17 +0800

    7. 整数反转

--- src/com/yitu/数字/_7_整数反转.java
+++ src/com/yitu/数字/_7_整数反转.java
@@ -4,7 +4,7 @@ package com.yitu.数字;
  * https://leetcode-cn.com/problems/reverse-integer/
  */
 public class _7_整数反转 {
-    public static int reverse(int x) {
+    public static int reverse1(int x) {
         long result=0;
         while(x!=0){
             result=result*10+x%10;
@@ -12,6 +12,18 @@ public class _7_整数反转 {
         }
         if(result>Integer.MAX_VALUE||result<Integer.MIN_VALUE)return 0;
         return (int) result;
+    }
+
+    public static int reverse(int x){
+        int res = 0;
+        while (x != 0) {
+            int prevRes = res;
+            int mod = x % 10;
+            res = prevRes * 10 + mod;
+            if ((res - mod) / 10 != prevRes) return 0;
+            x /= 10;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
