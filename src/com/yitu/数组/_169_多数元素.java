8c4940a21652d54b67460d87fc8128c11576a789
Wang Jie <2551434573@qq.com>
2021-11-13 01:38:24 +0800

    169. 多数元素

--- src/com/yitu/数组/_169_多数元素.java
+++ src/com/yitu/数组/_169_多数元素.java
@@ -1,34 +1,20 @@
 package com.yitu.数组;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * https://leetcode-cn.com/problems/majority-element/
  */
 public class _169_多数元素 {
-    private Map<Integer, Integer> countNums(int[] nums) {
-        Map<Integer, Integer> counts = new HashMap<>();
-        for (int num : nums) {
-            if (!counts.containsKey(num)) {
-                counts.put(num, 1);
-            } else {
-                counts.put(num, counts.get(num) + 1);
-            }
-        }
-        return counts;
-    }
-
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> counts = countNums(nums);
-
-        Map.Entry<Integer, Integer> majorityEntry = null;
-        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
-                majorityEntry = entry;
+        int cnt = 0, major = 0;
+        for (int num : nums) {
+            if (cnt == 0) {
+                major = num;
+                cnt = 1;
+            } else {
+                cnt += (major == num ? 1 : -1);
             }
         }
-
-        return majorityEntry.getKey();
+        return major;
     }
 }
