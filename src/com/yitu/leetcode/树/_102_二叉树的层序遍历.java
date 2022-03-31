06f5c9ae993110e99f44a464dc875f72638e5028
Wang Jie <2551434573@qq.com>
2022-03-31 19:30:12 +0800

    将二叉树层序遍历代码格式化

--- src/com/yitu/leetcode/树/_102_二叉树的层序遍历.java
+++ src/com/yitu/leetcode/树/_102_二叉树的层序遍历.java
@@ -13,26 +13,26 @@ import java.util.Queue;
  */
 public class _102_二叉树的层序遍历 {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list=new ArrayList<>();
-        if(root==null) return list;
-        Queue<TreeNode> queue=new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) return list;
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        int levelSize=1;
-        List<Integer> layerList=new ArrayList<>();
-        while (!queue.isEmpty()){
+        int levelSize = 1;
+        List<Integer> layerList = new ArrayList<>();
+        while (!queue.isEmpty()) {
             TreeNode node = queue.poll();
             layerList.add(node.val);
             levelSize--;
-            if(node.left!=null){
+            if (node.left != null) {
                 queue.add(node.left);
             }
-            if(node.right!=null){
+            if (node.right != null) {
                 queue.add(node.right);
             }
-            if(levelSize==0){
+            if (levelSize == 0) {
                 list.add(layerList);
-                levelSize=queue.size();
-                layerList=new ArrayList<>();
+                levelSize = queue.size();
+                layerList = new ArrayList<>();
             }
         }
         return list;
