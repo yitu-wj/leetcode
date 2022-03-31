b05dc6e4bf6e321878e7441f23559c916bf601f1
Wang Jie <2551434573@qq.com>
2022-03-31 19:17:45 +0800

    迭代方式实现中序遍历 用栈模拟方法的调用

--- src/com/yitu/leetcode/树/_94_二叉树的中序遍历.java
+++ src/com/yitu/leetcode/树/_94_二叉树的中序遍历.java
@@ -5,6 +5,7 @@ import com.yitu.leetcode.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
@@ -14,21 +15,37 @@ public class _94_二叉树的中序遍历 {
      * 递归的方式实现中序遍历
      */
     public List<Integer> inorderTraversal1(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
-        inorder1(list,root);
+        List<Integer> list = new ArrayList<>();
+        inorder1(list, root);
         return list;
     }
-    public void inorder1(List<Integer> list,TreeNode node){
-        if(node==null)return;
-        inorder1(list,node.left);
+
+    public void inorder1(List<Integer> list, TreeNode node) {
+        if (node == null) return;
+        inorder1(list, node.left);
         list.add(node.val);
-        inorder1(list,node.right);
+        inorder1(list, node.right);
     }
 
     /**
-     * 迭代方式实现中序遍历
+     * 迭代方式实现中序遍历 用栈模拟方法的调用
      */
-    public List<Integer> inorderTraversal2(TreeNode root){
-        return null;
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
