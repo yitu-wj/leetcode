a83637708a71a1dfbf8be25671e591baca1f279c
Wang Jie <2551434573@qq.com>
2022-03-31 19:22:51 +0800

    迭代方式实现后序遍历 用栈模拟方法的调用

--- src/com/yitu/leetcode/树/_145_二叉树的后序遍历.java
+++ src/com/yitu/leetcode/树/_145_二叉树的后序遍历.java
@@ -5,20 +5,51 @@ import com.yitu.leetcode.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
  */
 public class _145_二叉树的后序遍历 {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
-        postorder(list,root);
+        List<Integer> list = new ArrayList<>();
+        postorder(list, root);
         return list;
     }
-    public void postorder(List<Integer> list,TreeNode node){
-        if(node==null) return;
-        postorder(list,node.left);
-        postorder(list,node.right);
+
+    public void postorder(List<Integer> list, TreeNode node) {
+        if (node == null) return;
+        postorder(list, node.left);
+        postorder(list, node.right);
         list.add(node.val);
+    }
+
+    /**
+     * 迭代方式实现后序遍历 用栈模拟方法的调用
+     */
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return res;
     }
 }
