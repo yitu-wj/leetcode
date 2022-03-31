48b3e8320ac30658b1223893f3625a3e51c2d9f2
Wang Jie <2551434573@qq.com>
2022-03-31 17:05:49 +0800

    修复栈实现前序遍历BUG

--- src/com/yitu/leetcode/树/_144_二叉树的前序遍历.java
+++ src/com/yitu/leetcode/树/_144_二叉树的前序遍历.java
@@ -3,9 +3,7 @@ package com.yitu.leetcode.树;
 
 import com.yitu.leetcode.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
@@ -15,40 +13,43 @@ public class _144_二叉树的前序遍历 {
      * 递归方式实现前序遍历
      */
     public static List<Integer> preorderTraversal1(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
-        preorder1(list,root);
+        List<Integer> list = new ArrayList<>();
+        preorder1(list, root);
         return list;
     }
-    private static void preorder1(List<Integer> list,TreeNode node){
-        if(node==null) return;
+
+    private static void preorder1(List<Integer> list, TreeNode node) {
+        if (node == null) return;
         list.add(node.val);
-        preorder1(list,node.left);
-        preorder1(list,node.right);
+        preorder1(list, node.left);
+        preorder1(list, node.right);
     }
 
     /**
-     * 循环方式实现前序遍历
+     * 循环方式实现前序遍历，用栈模拟方法的调用
      */
-    public static List<Integer> preorderTraversal2(TreeNode root){
-        Stack<TreeNode> stack=new Stack<>();
-        List<Integer> list=new ArrayList<>();
-        stack.push(root);
-        while (!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            if(node==null) continue;
-            list.add(node.val);
-            if(node.left!=null) {
-                stack.add(node.left);
-            }
-            if(node.right!=null) {
-                stack.add(node.right);
-            }
+    public static List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
         }
-        return list;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                res.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        TreeNode root=new TreeNode(3,new TreeNode(1,null,new TreeNode(2,null,null)),null);
+        TreeNode root = new TreeNode(3, new TreeNode(1, null, new TreeNode(2, null, null)), null);
         List<Integer> list1 = preorderTraversal1(root);
         System.out.println(list1);
         List<Integer> list2 = preorderTraversal2(root);
