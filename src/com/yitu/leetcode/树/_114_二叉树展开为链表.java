b6404fd6212b8db8628bd9d5e467b63ff2f4b3f6
Wang Jie <2551434573@qq.com>
2022-04-03 14:45:20 +0800

    114. 二叉树展开为链表

---
+++ src/com/yitu/leetcode/树/_114_二叉树展开为链表.java
@@ -0,0 +1,35 @@
+package com.yitu.leetcode.树;
+
+import com.yitu.leetcode.TreeNode;
+
+/**
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
+ */
+public class _114_二叉树展开为链表 {
+    TreeNode listTailNode;
+
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        TreeNode headNode = new TreeNode();
+        listTailNode = headNode;
+        flatten2(root);
+        root.left = null;
+        root.right = headNode.right.right;
+    }
+
+    public void flatten2(TreeNode root) {
+        if (root == null) return;
+        TreeNode newNode = new TreeNode(root.val);
+        listTailNode.right = newNode;
+        listTailNode = newNode;
+        flatten2(root.left);
+        flatten2(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1, new TreeNode(2), new TreeNode(5));
+        new _114_二叉树展开为链表().flatten(node);
+        System.out.println(node);
+
+    }
+}
