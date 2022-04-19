24a7b165f5f834c36881035da3f1ab4b65ed496f
Wang Jie <2551434573@qq.com>
2022-04-19 21:11:09 +0800

    111. 二叉树的最小深度

---
+++ src/com/yitu/leetcode/树/_111_二叉树的最小深度.java
@@ -0,0 +1,72 @@
+package com.yitu.leetcode.树;
+
+import com.yitu.leetcode.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+ */
+public class _111_二叉树的最小深度 {
+    /**
+     * 深度优先搜索
+     */
+    public int minDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int minDepth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            minDepth = Math.min(minDepth1(root.left), minDepth);
+        }
+        if (root.right != null) {
+            minDepth = Math.min(minDepth1(root.right), minDepth);
+        }
+
+        return minDepth + 1;
+    }
+
+    class QueueNode {
+        TreeNode node;
+        int depth;
+
+        public QueueNode(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+
+    /**
+     * 广度优先搜索
+     */
+    public int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<QueueNode> queue = new LinkedList<QueueNode>();
+        queue.offer(new QueueNode(root, 1));
+        while (!queue.isEmpty()) {
+            QueueNode nodeDepth = queue.poll();
+            TreeNode node = nodeDepth.node;
+            int depth = nodeDepth.depth;
+            if (node.left == null && node.right == null) {
+                return depth;
+            }
+            if (node.left != null) {
+                queue.offer(new QueueNode(node.left, depth + 1));
+            }
+            if (node.right != null) {
+                queue.offer(new QueueNode(node.right, depth + 1));
+            }
+        }
+
+        return 0;
+    }
+}
