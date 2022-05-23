b574518241baac140cbd10929010e94925c3f7d4
Wang Jie <2551434573@qq.com>
2022-05-23 09:04:05 +0800

    349. 两个数组的交集

---
+++ src/com/yitu/leetcode/数组/_349_两个数组的交集.java
@@ -0,0 +1,35 @@
+package com.yitu.leetcode.数组;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * https://leetcode.cn/problems/intersection-of-two-arrays/
+ */
+public class _349_两个数组的交集 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> set2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        return getIntersection(set1, set2);
+    }
+
+    public int[] getIntersection(Set<Integer> set1, Set<Integer> set2) {
+        if (set1.size() > set2.size()) {
+            return getIntersection(set2, set1);
+        }
+        Set<Integer> intersectionSet = new HashSet<>();
+        for (int num : set1) {
+            if (set2.contains(num)) {
+                intersectionSet.add(num);
+            }
+        }
+        int[] intersection = new int[intersectionSet.size()];
+        int index = 0;
+        for (int num : intersectionSet) {
+            intersection[index++] = num;
+        }
+        return intersection;
+    }
+}
